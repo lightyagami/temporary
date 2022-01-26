28e2c8fb0244b269c91ce41503855a6242e3ea0e
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2022-01-26 21:29:02 +0530

    vayu: PEX: Jan build Unofficial

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/PixelExtended/manifest 
 git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b vayu_staging .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch aosp_vayu-user
 export PEX_BUILD_TYPE=OFFICIAL
@@ -11,8 +11,8 @@ export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 #OTA Json Generate#
-python3 OTA/support/ota.py
+# python3 OTA/support/ota.py
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy OTA/builds/vayu.json cirrus:vayu -P
+# rclone copy OTA/builds/vayu.json cirrus:vayu -P
