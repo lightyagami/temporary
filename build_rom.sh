e0ce2e7bba7683e848dd45f81bf08fb0ff189e80
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2022-01-27 07:35:05 +0530

    vayu: PEX: Rerun

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom 
 source build/envsetup.sh
 lunch aosp_vayu-user
-export PEX_BUILD_TYPE=OFFICIAL
+# export PEX_BUILD_TYPE=OFFICIAL
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
